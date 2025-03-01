83e15b076a6b66d27c922a00601c7608698d6629
Abdul Rahman <mrrehman002@gmail.com>
2025-03-01 21:13:50 +0500

    creating webhook

--- lib/database/moongoose.ts
+++ lib/database/moongoose.ts
@@ -6,6 +6,9 @@ interface MongooseConnection {
     conn: Mongoose | null;
     promise: Promise<Mongoose> | null;
 }
+declare global {
+    var mongoose: MongooseConnection | undefined;
+  }
 
 let cached: MongooseConnection = (global as any).mongoose || {};
 
