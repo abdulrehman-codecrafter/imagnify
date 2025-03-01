31d3cc431efa2b24c1ff4f6f7d6c4af66d324ece
Abdul Rahman <mrrehman002@gmail.com>
2025-03-01 22:48:54 +0500

    creating webhook

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,8 @@
+import { connectToDatabase } from '@/lib/database/moongoose'
 import React from 'react'
 
 export default function Home() {
+    connectToDatabase()
   return (
     <div>
         <h1>Home</h1>
